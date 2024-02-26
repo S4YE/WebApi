00cea7f8348aab6f70ae10f96cd0f8b53fe81675
SYE ♦ <kaan_1635@hotmail.com>
2024-02-26 18:54:17 +0300

    Add project files.

---
+++ Bussiness/Abstracts/IBrandService.cs
@@ -0,0 +1,17 @@
+﻿using Bussiness.Dtos.Requests;
+using Bussiness.Dtos.Responses;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Abstracts;
+
+public interface IBrandService
+{
+    CreatedBrandResponse Add(CreateBrandRequest createBrandRequest);
+    List<GetAllBrandResponse> GetAll();
+}
+//responses and requests
---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,43 @@
+﻿using Bussiness.Abstracts;
+using Bussiness.Dtos.Requests;
+using Bussiness.Dtos.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+        
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateBrandRequest createBrandRequest)
+        {
+            CreatedBrandResponse createdBrandResponse = _brandService.Add(createBrandRequest);
+
+            return Ok(createdBrandResponse);
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_brandService.GetAll());
+        }
+
+    }
+}
+
+
+//404 - Hata
+//401 - Yetkisiz bir islem 
+//200 - basarili 
+//201 - basarili ve eklendi
